94816a5fdc4b0206fa9cb291cadc1fe7dbfc9719
Flemming Madsen <git@madsensoft.dk>
2016-05-02 11:17:46 +0200

    Use pathogen for plugins

--- .vim/INSTALL.sh
+++ .vim/INSTALL.sh
@@ -1,3 +1,5 @@
+mkdir -p bundle
+cd bundle
 git clone https://github.com/vim-scripts/matchit.zip
 git clone https://github.com/JalaiAmitahl/maven-compiler.vim
 git clone https://github.com/junegunn/vim-easy-align
