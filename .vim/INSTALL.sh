b65785dad24b2d3fb72590d63dc34832780a4f19
Flemming Madsen <git@madsensoft.dk>
2016-05-02 12:06:06 +0200

    .vim

--- .vim/INSTALL.sh
+++ .vim/INSTALL.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
 mkdir -p bundle
 cd bundle
-git clone https://github.com/vim-scripts/matchit.zip
-git clone https://github.com/JalaiAmitahl/maven-compiler.vim
-git clone https://github.com/junegunn/vim-easy-align
+(cat << EOF 
+    https://github.com/vim-scripts/matchit.zip
+    https://github.com/JalaiAmitahl/maven-compiler.vim
+    https://github.com/junegunn/vim-easy-align
+EOF
+) | \
+while read rep ;do
+    dir=${rep##*/}
+    echo "* $dir - $rep"
+    if [[ -d $dir ]] ;then
+        git -C $dir pull
+    else
+        git clone $rep
+    fi
+done
