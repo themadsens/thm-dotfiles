82634e1c32446ed3756082b482b561e03240d766
Flemming Madsen <git@madsensoft.dk>
2016-10-28 12:12:07 +0200

    push

---
+++ bin/ipinfo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+result() {
+	mawk 'd==0 {printf("RES:%02d %s\n", NR, $0) >"/dev/stderr"} d==1 {print} /^\r$/ {d=1}' | jq
+}
+
+nc ipinfo.io 80 << EOF | result
+GET / HTTP/1.1
+Host: ipinfo.io
+
+EOF
