c7da9a85c62e1e4763f2d4f305dea5acd6f17ca7
Flemming Madsen <fm@fmmbp.amplexfil.dk>
2016-11-15 20:54:24 +0100

    push

--- .vim/INSTALL.sh
+++ .vim/INSTALL.sh
@@ -9,7 +9,7 @@ cd bundle
     https://github.com/othree/yajs.vim
     https://github.com/othree/javascript-libraries-syntax.vim
 	https://github.com/udalov/kotlin-vim
-	https://github.com/scrooloose/syntastic.git
+#	https://github.com/scrooloose/syntastic.git
 EOF
 ) | grep -v '^ *#'| \
 while read rep ;do
