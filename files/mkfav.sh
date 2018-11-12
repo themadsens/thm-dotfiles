d38963df7b7d14f1def5bb62ad773f12e2919f6c
Flemming Madsen <flemming@themadsens.dk>
2018-11-12 16:16:43 +0100

    push

---
+++ files/mkfav.sh
@@ -0,0 +1,9 @@
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 34 -draw 'text 2,33 Pr' prod.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 34 -draw 'text 2,33 Tg' Tg.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 30 -draw 'text -4,33 Cm' CM.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 34 -draw 'text -2,33 Ak' Akb.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 34 -draw 'text 0,33 Tg' Tg.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 30 -draw 'text -4,33 Dm' Dm.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 30 -draw 'text 2,33 Dv' Dv.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 34 -draw 'text 4,33 Ft' Ft.png
+gm convert -font helvetica-bold -fill yellow -size 40x40 xc:'#ffff8800' -pointsize 30 -draw 'text 2,33 Cd' Cd.png
